5cba3bf44a21a423ae8c41fd1ecf4616872ebe06
Arnold-HTH <clausurado_vitaminar@hotmail.com>
2023-04-02 12:54:41 -0500

    cambiar funcion async await por promesa nativa

---
+++ app2.js
@@ -0,0 +1,20 @@
+const lluviaDeIdeasCena = require('./library.js');
+
+
+// Native promise version:
+async function nativePromiseDinner() {
+  lluviaDeIdeasCena().then((comida) => {
+	  console.log(`voy a hacer${comida} para la cena.`);
+  });
+}
+
+
+// async/await version:
+function announceDinner() {
+   //Write your code below:
+   const comida = lluviaDeIdeasCena();
+   console.log('voy hacer ${comida} para la cena.');
+   //nativePromiseDinner();
+   announceDinner()
+  
+}
