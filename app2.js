687d7c81aa0d81c8274ddec6efbc805978329f56
Arnold-HTH <clausurado_vitaminar@hotmail.com>
2023-04-02 13:35:44 -0500

    tarea3

--- app2.js
+++ app2.js
@@ -1,36 +1,18 @@
 const lluviaDeIdeasCena = require(' ./library2.js');
 
-
 // Native promise version:
-function nativePromiseDinner() {
+async function nativePromiseDinner() {
   lluviaDeIdeasCena().then((comida) => {
-	  console.log("voy a hacer ${comida} para la cena.");
+	  console.log('voy a hacer ${comida} para la cena.');
   });
 }
 
 
 // async/await version:
-async function announceDinner() {
-   //Write your code below:
-   const comida = await lluviaDeIdeasCena();
-   console.log('voy hacer $[comida] para la cena.');
+function announceDinner() {
+//Write your code below:
+   const comida = lluviaDeIdeasCena();
+   console.log('voy hacer ${comida} para la cena.');
 }
 //nativePromiseDinner();
-announceDinner();
-//
-// Native promise version:
-// async function nativePromiseDinner() {
-//   lluviaDeIdeasCena().then((comida) => {
-// 	  console.log(`voy a hacer${comida} para la cena.`);
-//   });
-// }
-
-
-// async/await version:
-// function announceDinner() {
-//    //Write your code below:
-//    const comida = lluviaDeIdeasCena();
-//    console.log('voy hacer ${comida} para la cena.');
-//    //nativePromiseDinner();
-//    announceDinner()
-// }
+   announceDinner()
